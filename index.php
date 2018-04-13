156c8ffa9db26b65deac9f2ea07fdeaf13853892
Simon Montoya Palacio <simon.montoya@ubiquom.com>
2018-04-13 08:34:23 -0500

    second commit

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
+$data = json_decode(file_get_contents('php://input'), true);
+	foreach($data['messages'] as $message){ // Echo every message
+		// Handle every message here
+		// Add to the database or generate a response
+		var_dump($message);
+	}
+
 // Process only when method is POST
-if($method == 'POST'){
-	$requestBody = file_get_contents('php://input');
+
+/*if($method == 'POST')
+{
+	 $requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
 	$text = $json->result->parameters->text;
@@ -31,11 +40,11 @@ if($method == 'POST'){
 	$response->speech = $speech;
 	$response->displayText = $speech;
 	$response->source = "webhook";
-	echo json_encode($response);
+	echo json_encode($response); 
 }
 else
 {
 	echo "Method not allowed";
-}
+}*/
 
 ?>
