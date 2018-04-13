420d35fafbcff9b99f00daf6509c1db826fa6b68
Simon Montoya Palacio <simon.montoya@ubiquom.com>
2018-04-13 09:00:54 -0500

    five commit

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php 
 
 $method = $_SERVER['REQUEST_METHOD'];
+// detalles de la conexion
+$conn_string = "host=ec2-54-75-239-237.eu-west-1.compute.amazonaws.com dbname=d8smd3t2n4dmq0 user=uqimauzdvpykbo port=5432 password=331313e8d38e7fc622232b901448a571ef9d399a3746bd52db652f3d0c658576 options='--client_encoding=UTF8'";
+
+// establecemos una conexion con el servidor postgresSQL
+$dbconn = pg_connect($conn_string);
+
+// Revisamos el estado de la conexion en caso de errores. 
+if(!$dbconn) {
+echo "Error: No se ha podido conectar a la base de datos\n";
+} else {
+echo "Conexión exitosa\n";
+}
+
+// Close connection
+pg_close($dbconn);
 
 $data = json_decode(file_get_contents('php://input'), true);
 var_dump($data);
